84df41738becd26f6d3a404b06975ad49eb1c7bd
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-10 16:54:57 +0530

    implementing data types for user

--- models/User.ts
+++ models/User.ts
@@ -1,1 +1,26 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, {Schema,model,models} from "mongoose";
+import bcrypt from "bcrypt";
+
+
+
+
+
+
+
+export interface IUser {
+    email: string;
+    password: string;
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+
+}
+const userSchema = new Schema<IUser> (
+    {
+        email:{type: String, required: true, unique:true},
+        password: {type: String, required:true }
+   },
+   {
+    timestamps: true
+   }
+);
