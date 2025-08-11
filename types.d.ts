d01ea202d06bc6caacf3efcef075bff1aad86dfa
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-11 23:43:37 +0530

    declareing global variable for mongoose connection

--- types.d.ts
+++ types.d.ts
@@ -1,1 +1,11 @@
 import { Connection } from "mongoose";
+
+
+declare  global {
+    var mangoose:{
+        conn: Connection | null;
+        promise:Promise<Connection> | null;
+    };
+}
+
+export{};
